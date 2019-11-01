c53fa496b8f548172f84a1c98070ef22fd248981
Volkan <volkan.guven@hotmail.se>
2019-11-01 23:16:01 +0100

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,6 @@
   https://www.codewars.com/kata/52449b062fb80683ec000024
 
   Trim and finally capitalize first letter of every word
+  Long solution
 */
 const generateHashtag = (str) => str.trim().replace(/\s+/g, ' ').length + 1 > 140 || str.trim().replace(/\s+/g, ' ').length < 1 ? false : "# ".concat(str).replace(/\s+/g, ' ').split(' ').map((a) => {return a.replace(/^\w/, (b) => {return b.toUpperCase()})}).join('');
